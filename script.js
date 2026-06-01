0a2eb43c43abb0d643b2db2d58319a15dbf575a3
Puja kumari <pujakumaripal96@gmail.com>
2026-06-01 23:55:36 +0530

    Fix API key error

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ async function getWeather() {
         return;
     }
 
-    const apiKey =  "YOUR_API_KEY;
-        
-
     const url =
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
